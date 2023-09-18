3016d35e7c1fe6fb1ed317eaef722c6a0a8a60d1
yoshiquest <yoshiquest@gmail.com>
2023-09-18 15:17:29 -0500

    Update config.

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_lSY29oIgn', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6bdtijh6494h71f83e047cvugi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_O6fC6swCF', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '36vghhu1p14q46c99v0chspb1q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://qmxmqwvsqg.execute-api.us-east-2.amazonaws.com/prod/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://ml99fq9ah5.execute-api.us-east-2.amazonaws.com/prod/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
